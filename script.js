0dd3e2cc0fc189baeb80c6085b96d3695de920d1
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-04-05 23:42:54 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log(is_date("October 13, 2014 11:13:00"));
+console.log(is_date(new Date(86400000)));
+console.log(is_date(new Date(99,5,24,11,33,30,0)));
+console.log(is_date([1, 2, 4, 0]));
